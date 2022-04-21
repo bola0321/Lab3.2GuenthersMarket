140aa6ce7139c8dc07cf20360cd6c7124027089a
Bolanle <bola0321@gmail.com>
2022-04-21 15:42:37 -0400

    actually fixed it. corrected lists, map, main, and methods

--- src/guentherMarket.java
+++ src/guentherMarket.java
@@ -1,76 +1,72 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class guentherMarket {
 
 	public static void main(String[] args) {
 
-		
 		// foodTracker.put
 		Scanner scnr = new Scanner(System.in);
 		String pickedProduce;
 		String yesNo;
 		ArrayList<String> produce = new ArrayList<String>();
 		ArrayList<Double> price = new ArrayList<Double>();
-		
-		
-		HashMap <String, Double>foodTracker = new HashMap <>();
-		foodTracker.put("pineapple",4.99);
-		foodTracker.put("mango",1.50);
+
+		HashMap<String, Double> foodTracker = new HashMap<>();
+		foodTracker.put("pineapple", 4.99);
+		foodTracker.put("mango", 1.50);
 		foodTracker.put("coconut", 4.49);
-		foodTracker.put("peach",0.99);
+		foodTracker.put("peach", 0.99);
 		foodTracker.put("broccoli", 1.09);
 		foodTracker.put("banana", 0.50);
-		foodTracker.put("green beans",3.99);
+		foodTracker.put("green beans", 3.99);
 		foodTracker.put("apple", 0.99);
 
-	
-
 		System.out.println("Welcome to Guenther's Market!");
-		
-		while(true) {
+
+		while (true) {
 			try {
 				System.out.println("");
 				menuHead();
 				menuFood();
-		
+
 				System.out.print("\n" + "What item would you like to order? ");
 				pickedProduce = scnr.nextLine().trim().toLowerCase();
-				
+
 				boolean userEntry = foodTracker.containsKey(pickedProduce);
-				if(userEntry == false) {
-					throw new RuntimeException ();
-					
+				if (userEntry == false) {
+					throw new NoSuchElementException();
+
 				}
-				
-				//int indexPlease = foodTracker.indexOf(pickedProduce);
-		
-				System.out.printf("Adding " + pickedProduce + " to cart at $" +"%.2f", foodTracker.get(pickedProduce));
-				
-				//change to ArrayList
-				//produce.get(pickedProduce, price.get(indexPlease));
-		
-				System.out.print("\n" +"\n"+ "Would you like to order anything else (y/n)? ");
+
+				produce.add(pickedProduce);
+				price.add(foodTracker.get(pickedProduce));
+
+				System.out.printf("Adding " + pickedProduce + " to cart at $" + "%.2f", foodTracker.get(pickedProduce));
+
+				System.out.print("\n" + "\n" + "Would you like to order anything else (y/n)? ");
 				yesNo = scnr.nextLine().trim().toLowerCase();
-				
+
 				if (yesNo.equals("n")) {
 					break;
 				}
-				
-				
-			}catch(Exception e) {
-				System.out.println(e);
+
+			} catch (NoSuchElementException e) {
+				System.out.println("Sorry, we don't have those.  Please try again.");
 			}
-			
-		} 
+
+		}
+
+		System.out.println("Thanks for your order!" +"\n");
+		System.out.println("Here's what you got: " + "\n");
+
+		// need to actually pass arraylist to method, not map ughhhhhhhhhhhh
+		finalList(produce, price);
+		System.out.println("\n" + "Average price per order is $" + average(price));
 		
-		System.out.println("Thanks for your order!");
-		System.out.println("Here's what you got: ");
-		
-		//need to actually pass arraylist to method, not map ughhhhhhhhhhhh
-		finalList(foodTracker);
-		
+
 	}
 
 	public static void menuHead() {
@@ -97,19 +93,49 @@ public class guentherMarket {
 		System.out.printf("%-15s", "apple");
 		System.out.println("$0.99");
 	}
-	//Needs to use the parallel arraylist instead
-	public static void finalList(HashMap<String,Double> map) {
-		for (String key: map.keySet()) {
-			System.out.printf("%-15s",key );
-			System.out.print( map.get(key) +"\n" );
-			
-		}
-	//}
-	//public void double average()
-//}
 
-	//public static void
-		
-}
+	public static void finalList(ArrayList<String> stringList, ArrayList<Double> doubleList) {
+		for (int i = 0; i <= stringList.size() - 1; i++) {
+			System.out.printf("%-15s", stringList.get(i));
+			System.out.print(doubleList.get(i));
+			System.out.println("");
+
+		}
+	}
+
+	public static double average(ArrayList<Double> doubleList) {
+		double sum = 0;
+		for (double x : doubleList) {
+			sum = sum + x;
+		}
+		double average = sum / (doubleList.size());
+		return average;
+	}
+
+	public static int highestCost(ArrayList<Double> doubleList) {
+		double high = 0;
+		int highIndex = 0;
+		for (int i = 0; i <= doubleList.size() - 1; i++) {
+			if (high < doubleList.get(i)) {
+				high = doubleList.get(i);
+				highIndex = i;
+			}
+		}
+		return highIndex;
+
+	}
 	
+	public static int lowestCost (ArrayList<Double> doubleList) {
+		double low = 0;
+		int lowIndex = 0;
+		for (int i = 0; i <= doubleList.size() - 1; i++) {
+			if (low > doubleList.get(i)) {
+				low = doubleList.get(i);
+				lowIndex = i;
+			}
+		}
+		return lowIndex;
+		
+	}
+
 }
